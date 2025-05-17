239d09b8bbbf831d030eae92866338bcfd38cfb4
r4mej <jemarcarlos06@gmail.com>
2025-05-17 18:09:37 +0800

    Removed src and put it to 05 Implementation folder

--- src/backend/models/Admin.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import User from './User.js';
-
-const adminSchema = new mongoose.Schema({
-  // If you have specific admin-only fields, add them here
-});
-
-export default User.discriminator('admin', adminSchema);
